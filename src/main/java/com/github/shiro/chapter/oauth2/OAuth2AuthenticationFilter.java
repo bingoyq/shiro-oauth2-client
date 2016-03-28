4da513bd27609542b3409b5a2bb9ed66431f024a
y.q <qin_easy@163.com>
2016-03-28 17:22:17 +0800

    修改跳转需要登录，直接使用参数验证

--- src/main/java/com/github/shiro/chapter/oauth2/OAuth2AuthenticationFilter.java
+++ src/main/java/com/github/shiro/chapter/oauth2/OAuth2AuthenticationFilter.java
@@ -16,105 +16,103 @@ import java.io.IOException;
 
 /**
  * 
- * @ClassName: OAuth2AuthenticationFilter 
- * @Description: TODO(用户鉴权，鉴权失败处理) 
- * @author 袁强 
- * @date 2016年3月24日 下午5:25:27 
+ * @ClassName: OAuth2AuthenticationFilter
+ * @Description: TODO(用户鉴权，鉴权失败处理)
+ * @author 袁强
+ * @date 2016年3月24日 下午5:25:27
  *
  */
 public class OAuth2AuthenticationFilter extends AuthenticatingFilter {
 
-    //oauth2 authc code参数名
-    private String authcCodeParam = "code";
-    //客户端id
-    private String clientId;
-    //服务器端登录成功/失败后重定向到的客户端地址
-    private String redirectUrl;
-    //oauth2服务器响应类型
-    private String responseType = "code";
+	// oauth2 authc code参数名
+	private String authcCodeParam = "code";
+	// 客户端id
+	private String clientId;
+	// 服务器端登录成功/失败后重定向到的客户端地址
+	private String redirectUrl;
+	// oauth2服务器响应类型
+	private String responseType = "code";
 
-    private String failureUrl;
+	private String failureUrl;
 
-    public void setAuthcCodeParam(String authcCodeParam) {
-        this.authcCodeParam = authcCodeParam;
-    }
+	public void setAuthcCodeParam(String authcCodeParam) {
+		this.authcCodeParam = authcCodeParam;
+	}
 
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
 
-    public void setRedirectUrl(String redirectUrl) {
-        this.redirectUrl = redirectUrl;
-    }
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
 
-    public void setResponseType(String responseType) {
-        this.responseType = responseType;
-    }
+	public void setResponseType(String responseType) {
+		this.responseType = responseType;
+	}
 
-    public void setFailureUrl(String failureUrl) {
-        this.failureUrl = failureUrl;
-    }
+	public void setFailureUrl(String failureUrl) {
+		this.failureUrl = failureUrl;
+	}
 
-    @Override
-    protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String code = httpRequest.getParameter(authcCodeParam);
-        return new OAuth2Token(code);
-    }
+	@Override
+	protected AuthenticationToken createToken(ServletRequest request, ServletResponse response) throws Exception {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String code = httpRequest.getParameter(authcCodeParam);
+		return new OAuth2Token(code);
+	}
 
-    @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
-        return false;
-    }
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+		return false;
+	}
 
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+	@Override
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
 
+		String error = request.getParameter("error");
+		String errorDescription = request.getParameter("error_description");
+		if (!StringUtils.isEmpty(error)) {// 如果服务端返回了错误
+			WebUtils.issueRedirect(request, response,
+					failureUrl + "?error=" + error + "error_description=" + errorDescription);
+			return false;
+		}
+		Subject subject = getSubject(request, response);
+		if (!subject.isAuthenticated()) {
+			if (StringUtils.isEmpty(request.getParameter(authcCodeParam))) {
+				// 如果用户没有身份验证，且没有auth code，则重定向到服务端授权
+				saveRequestAndRedirectToLogin(request, response);
+				return false;
+			}
+		}
+		return executeLogin(request, response);
+	}
 
-        String error = request.getParameter("error");
-        String errorDescription = request.getParameter("error_description");
-        if(!StringUtils.isEmpty(error)) {//如果服务端返回了错误
-            WebUtils.issueRedirect(request, response, failureUrl + "?error=" + error + "error_description=" + errorDescription);
-            return false;
-        }
+	@Override
+	protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request,
+			ServletResponse response) throws Exception {
+		issueSuccessRedirect(request, response);
+		return false;
+	}
 
-        Subject subject = getSubject(request, response);
-        if(!subject.isAuthenticated()) {
-            if(StringUtils.isEmpty(request.getParameter(authcCodeParam))) {
-                //如果用户没有身份验证，且没有auth code，则重定向到服务端授权
-                saveRequestAndRedirectToLogin(request, response);
-                return false;
-            }
-        }
-
-        return executeLogin(request, response);
-    }
-
-    @Override
-    protected boolean onLoginSuccess(AuthenticationToken token, Subject subject, ServletRequest request,
-                                     ServletResponse response) throws Exception {
-        issueSuccessRedirect(request, response);
-        return false;
-    }
-
-    @Override
-    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException ae, ServletRequest request,
-                                     ServletResponse response) {
-        Subject subject = getSubject(request, response);
-        if (subject.isAuthenticated() || subject.isRemembered()) {
-            try {
-                issueSuccessRedirect(request, response);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                WebUtils.issueRedirect(request, response, failureUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return false;
-    }
+	@Override
+	protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException ae, ServletRequest request,
+			ServletResponse response) {
+		Subject subject = getSubject(request, response);
+		if (subject.isAuthenticated() || subject.isRemembered()) {
+			try {
+				issueSuccessRedirect(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				WebUtils.issueRedirect(request, response, failureUrl);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
 
 }
